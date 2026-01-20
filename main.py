54aa7ccfed5f20d06c3dfa39f7d7cdab7e3eeebc
rithwik <rithwikvasa@gmail.com>
2026-01-20 18:28:32 +0530

    added the data transforamtion code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.basic_project import logger
 from src.basic_project.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.basic_project.pipeline.data_validation_pipeline import DataValidationTriningPipeline
+from src.basic_project.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 
 
 
@@ -22,6 +23,16 @@ try:
     data_validation=DataValidationTriningPipeline()
     data_validation.initiate_data_validation()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx=========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME="Date transformtion stage"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    data_transforamtion=DataTransformationTrainingPipeline()
+    data_transforamtion.initiate_data_transformation()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==============x")
 except Exception as e:
     logger.exception(e)
     raise e
